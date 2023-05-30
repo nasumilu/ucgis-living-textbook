506cf9da511ce0413a1eec2ef0c6eba457cad578
ohururo <r.o.ohuru@utwente.nl>
2023-05-30 15:40:36 +0200

    adding imagepath attribute to concept

--- src/Entity/Concept.php
+++ src/Entity/Concept.php
@@ -350,6 +350,7 @@ class Concept implements SearchableInterface, ReviewableInterface, IdInterface
    */
   private ?string $imagePath = null;
 
+<<<<<<< HEAD
   /**
      * @Assert\Image(
      *     maxSize="2M",
@@ -358,6 +359,8 @@ class Concept implements SearchableInterface, ReviewableInterface, IdInterface
      */
   private $imageFile;
 
+=======
+>>>>>>> a459b8e0 (adding imagepath attribute to concept)
   /** Concept constructor. */
   public function __construct()
   {
