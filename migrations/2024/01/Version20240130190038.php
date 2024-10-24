78dd0c17fc0f23f19ee5a8e142c2976813265458
Robert Ohuru <r.o.ohuru@utwente.nl>
2024-10-24 20:41:24 +0200

    Do not delete definition if exist

--- migrations/2024/01/Version20240130190038.php
+++ migrations/2024/01/Version20240130190038.php
@@ -21,7 +21,7 @@ final class Version20240130190038 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE data_definition (id INT AUTO_INCREMENT NOT NULL, text LONGTEXT DEFAULT NULL, created_at DATETIME NOT NULL, created_by VARCHAR(255) DEFAULT NULL, updated_at DATETIME DEFAULT NULL, updated_by VARCHAR(255) DEFAULT NULL, deleted_at DATETIME DEFAULT NULL, deleted_by VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE concept ADD definition_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE concept ADD definition_id INT DEFAULT NULL, ALTER COLUMN definition SET DEFAULT NULL');
         $this->addSql('ALTER TABLE concept ADD CONSTRAINT FK_E74A6050D11EA911 FOREIGN KEY (definition_id) REFERENCES data_definition (id)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_E74A6050D11EA911 ON concept (definition_id)');
     }
