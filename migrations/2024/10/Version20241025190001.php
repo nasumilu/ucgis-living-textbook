c33212eeb828581bf93d5779a18829c7836a7a08
Robert Ohuru <robertohuru@gmail.com>
2024-10-25 17:00:26 +0200

    set definition field to null

---
+++ migrations/2024/10/Version20241025190001.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Manually generated Migration to resolve the null value for definition. 
+ */
+final class Version20241025190001 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Set definition default to null';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE concept ALTER COLUMN definition SET DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE concept ALTER COLUMN definition LONGTEXT NOT NULL');
+    }
+
+    public function isTransactional(): bool
+    {
+        return false;
+    }
+}
