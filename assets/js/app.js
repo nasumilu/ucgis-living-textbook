90d8b348be53bc940ca0120ced4949382448441f
Michael Lucas <nasumilu@gmail.com>
2025-10-19 17:49:12 -0400

    removed sentry

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,26 +2,9 @@ require('../css/app.scss');
 
 // IE 11 polyfill
 import './ie11';
-// Sentry
-import * as Sentry from '@sentry/browser';
+
 // Import routing
 import './routing';
-
-
-// Only bind when production mode is set
-if (window.SENTRY_DSN) {
-  // Create the default sentry client
-  // This instance will communicate any default JS errors
-  Sentry.init({
-    dsn: window.SENTRY_DSN,
-    release: window.SENTRY_RELEASE,
-  });
-
-  Sentry.setUser({
-    username: window.SENTRY_USERNAME,
-    ip_address: window.SENTRY_IP_ADDRESS,
-  });
-}
 
 // Create global $ and jQuery variables
 const $ = require('jquery');
